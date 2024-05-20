46706f59f351d3e8cc2fbb7336c6361213715448
김종덕 <kjd742388@gmail.com>
2024-05-20 15:06:07 +0900

    상세 페이지 계산 알고리즘 추가

--- final-back/cards/urls.py
+++ final-back/cards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='cards'
 urlpatterns = [
     path('<str:username>/update-card-list/', views.update_card_list),
-    path('<str:username>/<int:card_id>/likes_card_toggle/', views.likes_card_toggle)
-
+    path('<str:username>/<int:card_id>/likes_card_toggle/', views.likes_card_toggle),
+    path('<int:card_id>/card_detail/', views.card_detail),
 ]
