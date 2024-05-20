257e6bf4f1da515da1b3ea4d1f5590edc87e997c
김종덕 <kjd742388@gmail.com>
2024-05-20 16:27:56 +0900

    상세 알고리즘 수정

--- final-back/cards/views.py
+++ final-back/cards/views.py
@@ -158,7 +158,11 @@ def detail_calculate(card_data):
         change_money['category'] = 1
     else:
         change_money['category'] = 2
-    change_money['detail'] = {'detail' : list(card_data['benefit'][0]['details'].split('\r\n'))}
+    detail_list = list(card_data['benefit'][0]['details'].split('\r\n'))
+    change_money['detail'] = dict()
+    for i in detail_list:
+        key, value = i.split(' : ')
+        change_money['detail'][key] = value
                 
     return change_money
 
