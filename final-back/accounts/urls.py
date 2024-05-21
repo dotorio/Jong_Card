7c37097d7384140038ceddc2dcddd2b751b19222
김종덕 <kjd742388@gmail.com>
2024-05-21 17:54:22 +0900

    데이터 추가

--- final-back/accounts/urls.py
+++ final-back/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='user'
 urlpatterns = [
     path('', views.index),
-    path('<str:username>/info/', views.user_info)
+    path('<str:username>/info/', views.user_info),
+    path('<str:username>/update/', views.update),
+
 ]
