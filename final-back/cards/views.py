858dce0ba55c9bb8bdba1cb8f1d910da0663d725
김종덕 <kjd742388@gmail.com>
2024-05-22 09:32:47 +0900

    추천 알고리즘 수정

--- final-back/cards/views.py
+++ final-back/cards/views.py
@@ -52,7 +52,7 @@ def get_recommend_cards(username):
     # 2. 그 카드를 좋아요 누른 유저들 조사
     related_user_ids = set()
     for card in liked_cards:
-        related_user_ids.update(card.like_users.exclude(id=user.id).values_list('id', flat=True))
+        related_user_ids.update(card.like_users.values_list('id', flat=True))
     
     if not liked_cards or not related_user_ids:
         # 유저가 좋아요한 카드가 없을 경우, 좋아요가 많은 카드 순으로 추천
@@ -71,6 +71,7 @@ def get_recommend_cards(username):
         user_liked_cards = Card.objects.filter(like_users__id=related_user_id)
         for card in user_liked_cards:
             weight_counter[card.id] += 1
+    print(weight_counter)
 
     # 4. 가중치가 높은 순서대로 카드 추천
     recommended_cards = [Card.objects.get(id=card_id) for card_id, _ in weight_counter.most_common()]
